81cdadce0314caef18afb72c0620d673eb5727e6
Tomas <254007@via.dk>
2018-02-14 11:14:47 +0100

    p

---
+++ Historical Art Gallary/Assets/Assets/Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    [SerializeField]
+    private float speed = 1f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f * speed;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+
+        /*if(Input.GetKey("w"))
+        {
+            transform.position += Vector3.forward*speed*Time.deltaTime;
+        }
+        if(Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if(Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if(Input.GetKey("s"))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }*/
+    }
+}
